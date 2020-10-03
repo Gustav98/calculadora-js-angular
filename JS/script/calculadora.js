e64c0e4a59245b4270f7286032120e47bb590586
Gustav98 <gustavo.oliveira141998@hotmail.com>
2020-10-02 21:12:26 -0300

    criando e estilizando calculadora em js

---
+++ JS/script/calculadora.js
@@ -0,0 +1,72 @@
+var numero = '';
+var displayBuffer = "";
+var termos = [undefined, undefined, undefined];
+var resultado = undefined;
+
+function limparTudo(){
+	limparMemoria();
+	limpaVisor();
+}
+
+function limparMemoria(){
+	numero = '';
+	termos = [undefined, undefined, undefined];
+	resultado = undefined;
+}
+
+function limpaVisor(){
+	displayBuffer = "";
+	let tela = document.getElementById("visor");
+	tela.value = displayBuffer
+}
+
+function mostraNoVisor(conteudo) {
+	displayBuffer = displayBuffer.concat(conteudo);
+	let tela = document.getElementById("visor");
+	tela.value = displayBuffer;
+}
+
+function pressionaNumero(num){
+	numero = numero.concat(num);
+	mostraNoVisor(num);
+}
+
+function pressionaOperacao(op){
+	if (termos[1] == undefined) {
+		termos[0] = numero;
+		termos[1] = op;
+		mostraNoVisor(op);
+		numero = "";
+	}
+}
+
+function pressionaIgual() {
+	if (termos[0] != undefined && termos[1] != undefined && numero != "") {
+		
+		termos[2] = numero;
+		var mantemResultado;
+	
+		switch (termos[1]) {
+			case "+":
+				resultado = Number(termos[0]) + Number(termos[2]);
+				break;
+	
+			case "-":
+				resultado = Number(termos[0]) - Number(termos[2]);
+				break;
+	
+			case "X":
+				resultado = Number(termos[0]) * Number(termos[2]);
+				break;
+	
+			case "÷":
+				resultado = Number(termos[0]) / Number(termos[2]);
+				break;
+		}
+		mantemResultado = resultado
+		limpaVisor();
+		mostraNoVisor(resultado);
+		limparMemoria();
+		numero = mantemResultado
+	}
+}
