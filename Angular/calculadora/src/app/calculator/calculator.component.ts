af5e45044bcd97c098423c651de760463baa69ce
Gustav98 <gustavo.oliveira141998@hotmail.com>
2020-10-03 14:56:56 -0300

    Desenvolvendo funcionalidades da calculadora

--- Angular/calculadora/src/app/calculator/calculator.component.ts
+++ Angular/calculadora/src/app/calculator/calculator.component.ts
@@ -7,6 +7,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalculatorComponent implements OnInit {
 
+  numeroAtual = '0';
+  primeiroNumero = null;
+  operador = null;
+  aguardandoSegundoNumero = false;
+
+  public getNumero(v: string){
+    if(this.aguardandoSegundoNumero){
+      this.numeroAtual = v;
+      this.aguardandoSegundoNumero = false;
+    } else {
+      this.numeroAtual === '0' ? this.numeroAtual = v: this.numeroAtual += v;
+    }
+  }
+
+  private fazCalculos(op, secondOp){
+    switch (op) {
+      case '+':
+        return this.primeiroNumero += secondOp;
+      case '-':
+        return this.primeiroNumero -= secondOp;
+      case '*':
+        return this.primeiroNumero *= secondOp;
+      case '/':
+        return this.primeiroNumero /= secondOp;
+      case '=':
+        return secondOp;
+    }
+  }
+
+  public getOperacao(op: string){
+    if(this.primeiroNumero === null){
+      this.primeiroNumero = Number(this.numeroAtual);
+    }else if(this.operador){
+      const resultado = this.fazCalculos(this.operador, Number(this.numeroAtual))
+      this.numeroAtual = String(resultado);
+      this.primeiroNumero = resultado;
+    }
+    this.operador = op;
+    this.aguardandoSegundoNumero = true;
+  }
+
+  public limpar(){
+    this.numeroAtual = '0';
+    this.primeiroNumero = null;
+    this.operador = null;
+    this.aguardandoSegundoNumero = false;
+  }
+
+
   constructor() { }
 
   ngOnInit(): void {
